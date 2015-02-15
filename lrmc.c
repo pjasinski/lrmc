0914198dd80dede3e879adbb1b9ee5bbc0b24887
pjasinski <patrick.jasinski6800@me.com>
2015-02-15 18:24:41 -0500

    added stuff

--- lrmc.c
+++ lrmc.c
@@ -3,15 +3,14 @@ int main(void)
 {
   char fname[255];
   char lname[255];
-  char bill[255];
   int school;
   int committee;
-
-  FILE* csv = fopen("./lrmc.csv", "w");
   int a = 0;
+  FILE* csv = fopen("./lrmc.csv", "w");
+  fprintf(csv, "fname,lname,school,committee\n");
     if (csv != NULL)
       {
-	  while (a = 0)
+	while (a == 0)
 	    {
 	      printf("Enter first name: ");
 	      scanf("%s", fname);
@@ -20,11 +19,7 @@ int main(void)
 	      printf("Enter last name: ");
 	      scanf("%s", lname);
 	      fprintf(csv, "%s,", lname);
-      
-	      printf("Enter bill title: ");
-	      scanf("%s", bill);
-	      fprintf(csv, "%s,", bill);
-      
+            
 	      printf("Enter committe number: ");
 	      scanf("%d", &committee);
 	      findcommittee(csv, committee);
